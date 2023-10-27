8e9ee3f483188f7473a424c8a157005ee9327f60
Your Name <you@example.com>
2023-10-26 23:46:40 -0300

    feat(login): Adicionando autenticação de usuario nas rotas

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,19 @@ import { FornecedorModule } from './modules/fornecedor/fornecedor.module';
 import { ProdutoModule } from './modules/produto/produto.module';
 import { UsuarioModule } from './modules/usuario/usuario.module';
 import { PrismaService } from './prisma/prisma.service';
+import { JwtAuthGuard } from './modules/auth/jwt.auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [FornecedorModule, CategoriaModule, ProdutoModule, AuthModule, UsuarioModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService, 
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/modules/usuario/usuario.module.ts
+++ src/modules/usuario/usuario.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-    providers: [UsuarioService],
+    providers: [UsuarioService, PrismaService],
     exports: [UsuarioService],
     
 })
