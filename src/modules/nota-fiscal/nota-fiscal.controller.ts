cd9638b2633404d371ac31fe539f9e74599abeb0
kleuvyn <kleuvyn@hotmail.com>
2023-11-10 20:01:08 -0300

    refatorei o nota fiscal  e cliente  para que ele fique que nem o figma

--- src/modules/nota-fiscal/nota-fiscal.controller.ts
+++ src/modules/nota-fiscal/nota-fiscal.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, NotFoundException } from '@nestjs/common';
 import { NotaFiscalService } from './nota-fiscal.service';
 import { NotaFiscal } from '@prisma/client';
 
@@ -13,7 +13,11 @@ export class NotaFiscalController {
 
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<NotaFiscal> {
-    return this.notaFiscalService.findNotaFiscalById(parseInt(id, 10));
+    try {
+      return await this.notaFiscalService.findNotaFiscalById(parseInt(id, 10));
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
   }
 
   @Post()
@@ -23,11 +27,19 @@ export class NotaFiscalController {
 
   @Put(':id')
   async update(@Param('id') id: string, @Body() notaFiscalData: NotaFiscal): Promise<NotaFiscal> {
-    return this.notaFiscalService.updateNotaFiscal(parseInt(id, 10), notaFiscalData);
+    try {
+      return await this.notaFiscalService.updateNotaFiscal(parseInt(id, 10), notaFiscalData);
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
   }
 
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<void> {
-    this.notaFiscalService.deleteNotaFiscal(parseInt(id, 10));
+    try {
+      await this.notaFiscalService.deleteNotaFiscal(parseInt(id, 10));
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
   }
 }
--- src/modules/nota-fiscal/nota-fiscal.service.ts
+++ src/modules/nota-fiscal/nota-fiscal.service.ts
@@ -1,39 +1,50 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class NotaFiscalService {
 
-  constructor(private prisma: PrismaService) {
-  }
+  constructor(private prisma: PrismaService) {}
 
   async findAllNotasFiscais() {
     return this.prisma.notaFiscal.findMany();
   }
 
   async findNotaFiscalById(id: number) {
-    if (id === undefined || typeof id !== 'number') {
-      throw new Error('ID inválido!');
+    if (!id || typeof id !== 'number') {
+      throw new NotFoundException('ID inválido!');
     }
 
-    return this.prisma.notaFiscal.findUnique({
+    const notaFiscal = await this.prisma.notaFiscal.findUnique({
       where: { id },
     });
+
+    if (!notaFiscal) {
+      throw new NotFoundException('Nota fiscal não encontrada.');
+    }
+
+    return notaFiscal;
   }
 
   async createNotaFiscal(data: any) {
-
     return this.prisma.notaFiscal.create({
       data,
     });
   }
-  
 
   async updateNotaFiscal(id: number, data: any) {
-    if (id === undefined || typeof id !== 'number') {
-      throw new Error('ID inválido!');
+    if (!id || typeof id !== 'number') {
+      throw new NotFoundException('ID inválido!');
     }
+
+    const existingNotaFiscal = await this.prisma.notaFiscal.findUnique({
+      where: { id },
+    });
+
+    if (!existingNotaFiscal) {
+      throw new NotFoundException('Nota fiscal não encontrada.');
+    }
+
     if (data.dataEmissao) {
       throw new Error('Data de emissão não pode ser atualizada!');
     }
@@ -45,15 +56,16 @@ export class NotaFiscalService {
   }
 
   async deleteNotaFiscal(id: number) {
-    if (id === undefined || typeof id !== 'number') {
-      throw new Error('ID inválido!');
+    if (!id || typeof id !== 'number') {
+      throw new NotFoundException('ID inválido!');
     }
+
     const existingNotaFiscal = await this.prisma.notaFiscal.findUnique({
       where: { id },
     });
 
     if (!existingNotaFiscal) {
-      throw new Error('Nota fiscal não encontrada.');
+      throw new NotFoundException('Nota fiscal não encontrada.');
     }
 
     return this.prisma.notaFiscal.delete({
