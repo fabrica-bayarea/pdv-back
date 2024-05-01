6dbc50f361639efc505ef6d83fb32d6c974a9b30
elrate <contatogabrielvc@gmail.com>
2024-04-30 21:36:54 -0300

    Modificando a classe de Guard

--- src/modules/auth/jwt.auth.guard.ts
+++ src/modules/auth/jwt.auth.guard.ts
@@ -1,25 +1,17 @@
-import {
-  ExecutionContext,
-  Injectable
-} from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { AuthGuard } from '@nestjs/passport';
-import { IS_PUBLIC_KEY } from 'src/decorators/public.decorator';
-  
-  @Injectable()
-  export class JwtAuthGuard extends AuthGuard('jwt') {
-    constructor(private reflector: Reflector) {
-      super();
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+ constructor(private reflector: Reflector) {}
+
+ canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
     }
-    
-    canActivate(context: ExecutionContext) {
-      const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-        context.getHandler(),
-        context.getClass(),
-      ]);
-      if (isPublic) {
-        return true;
-      }
-      return super.canActivate(context);
-    }
-  }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return roles.includes(user.role);
+ }
+}
